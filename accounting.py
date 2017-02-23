21093eeb940968f494b50f3073d4cfdf6d25fc52
Katie <knt.taylor@gmail.com>
2017-02-23 04:15:39 +0000

    checking if underpaid or overpaid

--- accounting.py
+++ accounting.py
@@ -7,12 +7,16 @@ def find_underpaid_customers(customer_data_file):
         customer_tokens = line.split("|")
 
         customer_name = customer_tokens[1]
+        customer_first = customer_name.split(" ")[0]
         customer_melons = float(customer_tokens[2])
         customer_paid = float(customer_tokens[3])
         customer_expected = customer_melons * melon_cost
 
-        if customer_expected != customer_paid:
-            print customer_name + " paid %s, expected %s" % (customer_paid, customer_expected)
+        if customer_expected > customer_paid:
+            print customer_first + " has underrpaid - Paid: %s, Expected %s" % (customer_paid, customer_expected)
+        elif customer_expected < customer_paid:
+            print customer_first + " has overpaid - Paid: %s, Expected %s" % (customer_paid, customer_expected)
+
 
 
 find_underpaid_customers("customer-orders.txt")
